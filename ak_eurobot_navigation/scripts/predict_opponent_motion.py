b73bbec7a1b3fdd1842ab108596b6e082fb6836b
akpp213 <knapping@mit.edu>
2018-07-13 18:37:54 +0300

    Pretty good coordinated detection

--- ak_eurobot_navigation/scripts/predict_opponent_motion.py
+++ ak_eurobot_navigation/scripts/predict_opponent_motion.py
@@ -8,6 +8,7 @@ import numpy as np
 from threading import Lock
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import tf
 
 
 class PredictOpponentMotion:
@@ -18,8 +19,8 @@ class PredictOpponentMotion:
 
         self.TIMES = rospy.get_param("/predict_opponent_motion/TIMES", 10)
         # self.MULT = rospy.get_param("/predict_opponent_motion/MULT")
-        self.SENSITIVITY = rospy.get_param("/predict_opponent_motion/SENSITIVITY", 0.9)
-        self.SPECIFICITY = rospy.get_param("/predict_opponent_motion/SPECIFICITY", 0.9)
+        self.SENSITIVITY = rospy.get_param("/predict_opponent_motion/SENSITIVITY", 0.8)
+        self.SPECIFICITY = rospy.get_param("/predict_opponent_motion/SPECIFICITY", 0.8)
 
         self.x0 = None
         self.y0 = None
@@ -32,6 +33,8 @@ class PredictOpponentMotion:
         self.permissible_region = None
         self.temporal_occupancy_grid = None
         self.map_updated = False
+
+        self.listener = tf.TransformListener()
 
         self.fig = plt.figure()
         self.img = []
@@ -64,20 +67,16 @@ class PredictOpponentMotion:
 
     def detected_robot(self, msg):
         if self.map_updated:
-            # robot_centers = self.map_to_occupancy_grid(msg.points)
-            # centers = robot_centers
-            # for center in robot_centers:
-            #     centers = np.append(centers, [center + 1], axis=0)
-            #     centers = np.append(centers, [center - 1], axis=0)
-            #     centers = np.append(centers, [center + np.array([0, 1])], axis=0)
-            #     centers = np.append(centers, [center + np.array([0, -1])], axis=0)
-            #     centers = np.append(centers, [center + np.array([1, 0])], axis=0)
-            #     centers = np.append(centers, [center + np.array([-1, 0])], axis=0)
-            # mask = np.zeros(self.temporal_occupancy_grid.shape[1:], dtype=bool)
-            # print centers
-            # mask[centers[:, 0], centers[:, 1]] = True
-            # print msg.points
-            mask = self.our_robot_circle(msg.points[-1].z/2.0, msg.points[-1])
+            robot_centers = self.map_to_occupancy_grid(msg.points)
+            main, secondary = self.our_poses()
+            if main is None or secondary is None:
+                return
+            centers = robot_centers[~((np.linalg.norm(robot_centers[:, :2]-main[:2], axis=1) <= 0.3) |
+                                    (np.linalg.norm(robot_centers[:, :2]-secondary[:2], axis=1) <= 0.3))]
+
+            mask = np.full(self.permissible_region.shape, False, dtype='bool')
+            for center in centers:
+                mask = mask | self.our_robot_circle(center[2]/2.0, center[:2])
             # print mask
             # P(state is occupied | observations) = P(state is occupied)*P(observations | state is occupied)/P(observations)
             # P(obs) = P(obs | state occupied) * P(state occupuied) + P(obs | state not occupied) * P(state not occupied)
@@ -93,49 +92,56 @@ class PredictOpponentMotion:
             # plt.pause(0.0001)
             self.img.append([plt.imshow(self.temporal_occupancy_grid[0])])
 
-    def map_to_occupancy_grid(self, pnts):
-        return np.array([(([pnt.x, pnt.y, pnt.z] - np.array([self.x0, self.y0, 0])) / self.resolution) for pnt in pnts]).astype(int)
+    def our_poses(self):
+        try:
+            (trans, rot) = self.listener.lookupTransform('/map', '/main_robot', rospy.Time(0))
+            yaw = tf.transformations.euler_from_quaternion(rot)[2]
+            state_main = np.array([trans[0], trans[1], yaw])
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.loginfo("Simulator failed to lookup tf for main robot")
+            return None, None
+
+        try:
+            (trans, rot) = self.listener.lookupTransform('/map', '/secondary_robot', rospy.Time(0))
+            yaw = tf.transformations.euler_from_quaternion(rot)[2]
+            state_secondary = np.array([trans[0], trans[1], yaw])
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.loginfo("Simulator failed to lookup tf for secondary robot")
+            return None, None
+
+        return state_main, state_secondary
+
+    @staticmethod
+    def map_to_occupancy_grid(pnts):
+        return np.array([[pnt.x, pnt.y, pnt.z] for pnt in pnts])
 
     def our_robot_circle(self, rad, coords):
-        # print rad
-        # print coords.x+self.x0
-        # print coords.y+self.y0
         # 'occupy' all cells
         robot = np.full(self.permissible_region.shape, True, dtype='bool')
-        # print robot.all()
-        # plt.imshow(robot)
-        # plt.pause(1)
 
         x, y = np.meshgrid(np.arange(0, self.permissible_region.shape[1]), np.arange(0, self.permissible_region.shape[0]))
 
         coords_prog = np.linspace(np.pi/2+.1, np.pi+.1, 8)
 
         # upper point
-        x1 = (coords.x + self.x0 - rad * np.sin(coords_prog)) / self.resolution
-        y1 = (coords.y + self.y0 + rad * np.cos(coords_prog)) / self.resolution
+        x1 = (coords[0] + self.x0 - rad * np.sin(coords_prog)) / self.resolution
+        y1 = (coords[1] + self.y0 + rad * np.cos(coords_prog)) / self.resolution
 
         # lower point
-        x2 = (coords.x + self.x0 + rad * np.sin(coords_prog)) / self.resolution
-        y2 = (coords.y + self.y0 - rad * np.cos(coords_prog)) / self.resolution
+        x2 = (coords[0] + self.x0 + rad * np.sin(coords_prog)) / self.resolution
+        y2 = (coords[1] + self.y0 - rad * np.cos(coords_prog)) / self.resolution
 
         # left point
-        x3 = (coords.x + self.x0 - rad * np.cos(coords_prog)) / self.resolution
-        y3 = (coords.y + self.y0 - rad * np.sin(coords_prog)) / self.resolution
+        x3 = (coords[0] + self.x0 - rad * np.cos(coords_prog)) / self.resolution
+        y3 = (coords[1] + self.y0 - rad * np.sin(coords_prog)) / self.resolution
 
         # right point
-        x4 = (coords.x + self.x0 + rad * np.cos(coords_prog)) / self.resolution
-        y4 = (coords.y + self.y0 + rad * np.sin(coords_prog)) / self.resolution
-
-        # print x1,x2,x3,x4
-        # print y1,y2,y3,y4
+        x4 = (coords[0] + self.x0 + rad * np.cos(coords_prog)) / self.resolution
+        y4 = (coords[1] + self.y0 + rad * np.sin(coords_prog)) / self.resolution
 
         # 'free' cells outside of each side of the robot
         for i, coord in enumerate(coords_prog):
             if i < len(coords_prog):
-                # plt.imshow(robot)
-                # print robot.any()
-                # print i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i
-                # plt.pause(1)
                 a = coord % (2 * np.pi)
                 if a < np.pi / 2 or a > 3 * np.pi / 2:
                     robot[y - y1[i] > (x - x1[i]) * np.tan(coord)] = False
@@ -149,29 +155,13 @@ class PredictOpponentMotion:
                 else:
                     robot[y - y3[i] > (x - x3[i]) * np.tan(np.pi / 2 + coord)] = False
                     robot[y - y4[i] < (x - x4[i]) * np.tan(np.pi / 2 + coord)] = False
-                # print robot.any()
-        # print robot.any()
-        # plt.imshow(robot)
-        # plt.pause(1)
 
         return robot
 
     def show_animation(self):
-    #     print self.i
-    #     self.fig = plt.figure()
-    #     self.im = plt.imshow(self.temporal_occupancy_grid[0], animated=True)
-    #     print "HI"
-    #     self.ani = FuncAnimation(self.fig, self.next_frame, frames=np.arange(self.i), blit=True)
-    #     plt.show()
-    #
-    # def next_frame(self, num):
-    #     self.im.append([self.temporal_occupancy_grid[num]])
-    #     print "Hello"
-    #     return self.im
-
         ani = animation.ArtistAnimation(self.fig, self.img, interval=20, blit=True, repeat_delay=0)
-        ani.save("updatingMap.mp4")
-        # plt.show()
+        # ani.save("updatingMap.mp4")
+        plt.show()
 
 
 if __name__ == "__main__":
--- ak_eurobot_navigation/scripts/spy.py
+++ ak_eurobot_navigation/scripts/spy.py
@@ -20,102 +20,120 @@ R = 0.08
 visualization = True
 
 
-def scan_callback(scan):
-    lidar_data = np.array([np.array(scan.ranges), scan.intensities]).T
-    landmarks = filter_scan(lidar_data)
+class Spy:
+    def __init__(self):
+        rospy.init_node("spy", anonymous=True)
 
-    if visualization == True:
-        # create and pub PointArray of detected beacon points
-        points = [Point(x=landmarks[i, 0], y=landmarks[i, 1], z=0) for i in range(len(landmarks))]
-        array = PointCloud(points=points)
+        self.R = rospy.get_param("R")
+        self.eps = rospy.get_param("clustering/eps")
+        self.min_samples = rospy.get_param("clustering/min_samples")
+        self.listener = tf.TransformListener()
+        self.last_main_landmarks = np.array([[], []]).T
+        self.last_secondary_landmarks = np.array([[], []]).T
+        # name = rospy.get_param("/robot_name")
+        # laser_min_angle = rospy.get_param("/secondary_robot/simulate/laser_min_angle")
+        # lidar_start_angle = rospy.get_param("/secondary_robot/lidar_a")
+        rospy.Subscriber("/secondary_robot/scan", LaserScan, self.scan_callback_secondary, queue_size=1)
+        rospy.Subscriber("/main_robot/scan", LaserScan, self.scan_callback_main, queue_size=1)
+        self.pub_center = rospy.Publisher("detected_robots", PointCloud, queue_size=1)
+        if visualization:
+            self.pub_landmarks = rospy.Publisher("landmarks", PointCloud, queue_size=1)
+
+    def scan_callback_main(self, scan):
+        self.scan_callback(scan, "main_robot")
+
+    def scan_callback_secondary(self, scan):
+        self.scan_callback(scan, "secondary_robot")
+
+    def scan_callback(self, scan, name):
+        lidar_data = np.array([np.array(scan.ranges), scan.intensities]).T
+        landmarks = self.filter_scan(lidar_data, name)
+        if name == "main_robot":
+            self.last_main_landmarks = landmarks.copy()
+            landmarks = np.concatenate((landmarks, self.last_secondary_landmarks))
+        else:
+            self.last_secondary_landmarks = landmarks.copy()
+            landmarks = np.concatenate((landmarks, self.last_main_landmarks))
+
+        if visualization:
+            # create and pub PointArray of detected beacon points
+            points = [Point(x=landmarks[i, 0], y=landmarks[i, 1], z=0) for i in range(len(landmarks))]
+            array = PointCloud(points=points)
+            array.header.frame_id = "map"
+            self.pub_landmarks.publish(array)
+
+        centers = []
+        if landmarks.shape[0] > 0:
+            # clustering
+            db = DBSCAN(eps=self.eps, min_samples=self.min_samples).fit(landmarks)
+            labels = db.labels_
+            unique_labels = set(labels)
+
+            for l in unique_labels:
+                if l == -1:
+                    # noise
+                    continue
+
+                class_member_mask = (labels == l)
+
+                group = landmarks[class_member_mask]
+                center = self.get_center(group)
+                # size = np.linalg.norm(group[-1] - group[0])
+                size = max(np.linalg.norm(group - group[::-1], axis=1))
+                centers.append(Point(x=center.x[0], y=center.x[1], z=size))
+
+        # create and pub PointArray of detected centers
+        array = PointCloud(points=centers)
         array.header.frame_id = "map"
-        pub_landmarks.publish(array)
+        array.header.stamp = rospy.Time.now()
+        self.pub_center.publish(array)
 
-    centers = []
-    if landmarks.shape[0] > 0:
-        # clustering
-        db = DBSCAN(eps=eps, min_samples=min_samples).fit(landmarks)
-        labels = db.labels_
-        unique_labels = set(labels)
+    def filter_scan(self, scan, name):
+        """Filters scan to get only landmarks (bright) located on field."""
+        loc = self.get_robot_loc(name)
+        if loc is None:
+            return np.array([np.array([]), np.array([])]).T
+        laser_min_angle = rospy.get_param("/"+name+"/simulate/laser_min_angle")
+        lidar_start_angle = rospy.get_param("/"+name+"/lidar_a")
+        ind = np.where(scan[:, 1] > MIN_INTENSITY)[0]
+        a = LIDAR_DELTA_ANGLE * ind + laser_min_angle
+        d = scan[ind, 0]
 
-        for l in unique_labels:
-            if l == -1:
-                # noise
-                continue
+        # x = d * np.cos(a + LIDAR_START_ANGLE) + LIDAR_X
+        # y = d * np.sin(a + LIDAR_START_ANGLE) + LIDAR_Y
 
-            class_member_mask = (labels == l)
+        x = d * np.cos(a + loc[2] + lidar_start_angle+.0) + loc[0]
+        y = d * np.sin(a + loc[2] + lidar_start_angle+.0) + loc[1]
 
-            group = landmarks[class_member_mask]
-            center = get_center(group)
-            size = np.linalg.norm(group[-1] - group[0])
-            centers.append(Point(x=center.x[0], y=center.x[1], z=size))
-        
-    # create and pub PointArray of detected centers
-    array = PointCloud(points=centers)
-    array.header.frame_id = "map"
-    array.header.stamp = rospy.Time.now()
-    pub_center.publish(array)
+        # inside field only
+        ind = np.where(np.logical_and(np.logical_and(x < FIELD_X + BEYOND_FIELD, x > -BEYOND_FIELD), np.logical_and(y < FIELD_Y + BEYOND_FIELD, y > - BEYOND_FIELD)))
 
+        x = x[ind]
+        y = y[ind]
+        return np.array([x, y]).T
 
-def filter_scan(scan):
-    """Filters scan to get only landmarks (bright) located on field."""
-    loc = get_robot_loc("secondary_robot")
-    if loc is None:
-        return np.array([np.array([]), np.array([])]).T
-    ind = np.where(scan[:, 1] > MIN_INTENSITY)[0]
-    a = LIDAR_DELTA_ANGLE * ind + laser_min_angle
-    d = scan[ind, 0]
-   
-    # x = d * np.cos(a + LIDAR_START_ANGLE) + LIDAR_X
-    # y = d * np.sin(a + LIDAR_START_ANGLE) + LIDAR_Y
+    def get_robot_loc(self, name):
+        try:
+            (trans, rot) = self.listener.lookupTransform('/map', '/' + name, rospy.Time(0))
+            yaw = tf.transformations.euler_from_quaternion(rot)[2]
+            state = np.array([trans[0], trans[1], yaw])
+            return state
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.loginfo("Simulator failed to lookup tf for " + name)
+            return None
 
-    x = d * np.cos(a + loc[2] + lidar_start_angle) + loc[0]
-    y = d * np.sin(a + loc[2] + lidar_start_angle) + loc[1]
+    def get_center(self, landmarks):
+        #bounds = (point - np.array([0.1, 0.1]), point + np.array([0.15, 0.15])
+        med = np.median(landmarks, axis=0)
+        dist = np.sum(med ** 2) ** .5
+        center_by_med = med + R * np.array([med[0] / dist, med[1] / dist])
+        center = least_squares(self.fun, center_by_med, args=[landmarks])#, loss="linear", bounds = bounds, args = [landmarks], ftol = 1e-3)
+        return center
 
-    # inside field only
-    ind = np.where(np.logical_and(np.logical_and(x < FIELD_X + BEYOND_FIELD, x > -BEYOND_FIELD), np.logical_and(y < FIELD_Y + BEYOND_FIELD, y > - BEYOND_FIELD)))
-
-    x = x[ind]
-    y = y[ind]
-    return np.array([x, y]).T
-
-
-def get_robot_loc(name):
-    try:
-        (trans, rot) = listener.lookupTransform('/map', '/' + name, rospy.Time(0))
-        yaw = tf.transformations.euler_from_quaternion(rot)[2]
-        state = np.array([trans[0], trans[1], yaw])
-        return state
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        rospy.loginfo("Simulator failed to lookup tf for " + name)
-        return None
-
-
-def get_center(landmarks):
-    #bounds = (point - np.array([0.1, 0.1]), point + np.array([0.15, 0.15])
-    med = np.median(landmarks, axis=0)
-    dist = np.sum(med ** 2) ** .5
-    center_by_med = med + R * np.array([med[0] / dist, med[1] / dist])
-    center = least_squares(fun, center_by_med, args=[landmarks])#, loss="linear", bounds = bounds, args = [landmarks], ftol = 1e-3)
-    return center
-
-
-def fun(point, landmarks):
-    return np.sum((landmarks - point) ** 2, axis=1) ** .5 - R
+    def fun(self, point, landmarks):
+        return np.sum((landmarks - point) ** 2, axis=1) ** .5 - R
 
 
 if __name__ == "__main__":
-    rospy.init_node("spy", anonymous=True)
-    R = rospy.get_param("R")
-    eps = rospy.get_param("clustering/eps")
-    min_samples = rospy.get_param("clustering/min_samples")
-    listener = tf.TransformListener()
-    # name = rospy.get_param("/robot_name")
-    laser_min_angle = rospy.get_param("/secondary_robot/simulate/laser_min_angle")
-    lidar_start_angle = rospy.get_param("/secondary_robot/lidar_a")
-    rospy.Subscriber("/scan", LaserScan, scan_callback, queue_size=1)
-    pub_center = rospy.Publisher("detected_robots", PointCloud, queue_size=1)
-    if visualization == True:
-        pub_landmarks = rospy.Publisher("landmarks", PointCloud, queue_size=1)
-
-    rospy.spin()
+    spy = Spy()
+rospy.spin()
